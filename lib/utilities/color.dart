0e086d8fb67614f9c815cdab48fff4e2e49fbe1e
Rifat Kautsar <kautsarrifat@gmail.com>
2023-09-18 16:28:10 +0700

    adding new color

--- lib/utilities/color.dart
+++ lib/utilities/color.dart
@@ -4,8 +4,11 @@ class ColorConst {
   static const button_color = Color(0xff2E3A59);
   static const blue_color = Color(0xff4F7FFA);
   static const blue_color2 = Color(0xff335FD1);
+  static const blue_color3 = Color(0xFF3C6EEF);
+  static const blue_color4 = Color(0xFF9AB6FF);
   static const black_color = Color(0xFF4C4E53);
   static const black_color2 = Color(0xFF0B1223);
   static const pink_color = Color(0xFFE7755C);
   static const blue_semi = Color(0xFFD2DFFF);
+  static const brown = Color(0xFF494343);
 }
