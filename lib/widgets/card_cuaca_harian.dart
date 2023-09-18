aae6217347736e8fe01ca2d04f9d69f9e9972474
Rifat Kautsar <kautsarrifat@gmail.com>
2023-09-18 16:55:48 +0700

    adding and refactoring some widget in card cuaca harian

--- lib/widgets/card_cuaca_harian.dart
+++ lib/widgets/card_cuaca_harian.dart
@@ -17,48 +17,68 @@ class CardCuacaHarian extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Container(
       height: 90.0,
-      child: Card(
+      padding: const EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
         color: ColorConst.blue_semi,
-        child: Container(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
+        borderRadius: BorderRadius.circular(10.0), // Adjust the border radius as needed
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(icon_cuaca),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        hari,
-                        style: const TextStyle(color: Colors.black),
+              ClipOval(
+                child: Container(
+                  width: 50.0,  // Adjust the size of the outer circle
+                  height: 50.0, // Adjust the size of the outer circle
+                  color: ColorConst.blue_color4,
+                  child: Center(
+                    child: ClipOval(
+                      child: Container(
+                        width: 30, // Adjust the size of the inner circle
+                        height: 30, // Adjust the size of the inner circle
+                        color: Colors.transparent, // Make the inner container transparent
+                        child: Image.asset(
+                          icon_cuaca,
+                          width: 30.0, // Adjust the size of the image
+                          height: 30.0, // Adjust the size of the image
+                        ),
                       ),
-                      Text(
-                        kondisi_cuaca,
-                        style: const TextStyle(color: Colors.black),
-                      ),
-                    ],
+                    ),
                   ),
-                ],
+                ),
               ),
-              Row(
+              const SizedBox(width: 15.0,),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    drajat_cuaca,
+                    hari,
                     style: const TextStyle(color: Colors.black),
                   ),
-                  const Icon(Icons.arrow_right)
+                  Text(
+                    kondisi_cuaca,
+                    style: const TextStyle(color: Colors.black),
+                  ),
                 ],
               ),
             ],
           ),
-        ),
+          Row(
+            children: [
+              Text(
+                drajat_cuaca,
+                style: const TextStyle(color: Colors.black),
+              ),
+              const Icon(Icons.arrow_right)
+            ],
+          ),
+        ],
       ),
     );
   }
