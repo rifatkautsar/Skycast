b30dcda87223781c243e63f1fd1f18a9960796ad
Rifat Kautsar <kautsarrifat@gmail.com>
2023-09-20 16:10:10 +0700

    adding controller weather data

---
+++ lib/controllers/weather_data_controller.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:get/get.dart';
+import '../service/api_client.dart';
+import '../models/weather_model.dart'; // Import your WeatherData model
+
+class WeatherDataController extends GetxController {
+  Rx<WeatherData?> weatherData = Rx<WeatherData?>(null);
+  Rx<List<Hour>?> forecastHourList = Rx<List<Hour>?>(null);
+
+  Future<void> fetchData(String location) async {
+    try {
+      final response = await ApiClient.fetchData(location);
+
+      if (response.statusCode == 200) {
+        final dynamic data = json.decode(response.body);
+        final weatherDataModel = WeatherData.fromJson(data);
+
+        // Assuming hourly forecast is directly under forecastday[0].hour
+          forecastHourList.value = weatherDataModel.forecast!.forecastday![0]?.hour;
+
+        weatherData.value = weatherDataModel;
+        print(response.body);
+      } else {
+        throw Exception("Failed to fetch data: ${response.statusCode}");
+      }
+    } catch (error) {
+      print("Exception occurred: $error");
+    }
+  }
+}
+
