4277661f4c58ffd7b9c4c5c5fe79111f3a17932f
Rifat Kautsar <kautsarrifat@gmail.com>
2023-09-24 13:49:41 +0700

    adding new method for making APU search request

--- lib/controllers/weather_data_controller.dart
+++ lib/controllers/weather_data_controller.dart
@@ -2,11 +2,29 @@ import 'dart:convert';
 
 import 'package:get/get.dart';
 import '../service/api_client.dart';
+import 'package:http/http.dart' as http;
 import '../models/weather_model.dart'; // Import your WeatherData model
 
 class WeatherDataController extends GetxController {
   Rx<WeatherData?> weatherData = Rx<WeatherData?>(null);
   Rx<List<Hour>?> forecastHourList = Rx<List<Hour>?>(null);
+
+  RxList<Map<String, dynamic>> locations = <Map<String, dynamic>>[].obs;
+
+  static Future<String?> searchLocation(String query) async {
+    String apiUrl =
+        "http://api.weatherapi.com/v1/search.json?key=2639061e276e41f89d934825231209&q=$query";
+
+    var response = await http.get(Uri.parse(apiUrl));
+
+    if (response.statusCode == 200) {
+      print(response.body);
+      return response.body;
+
+    } else {
+      throw Exception("Failed to fetch data: ${response.statusCode}");
+    }
+  }
 
   Future<void> fetchData(String location) async {
     try {
